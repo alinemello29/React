345f8b9abd128658b20f73c0b99bc51850c4fc3a
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-07-20 21:17:35 -0300

    Add files via upload

---
+++ Crie um site de informações do React/index1.js
@@ -0,0 +1,4 @@
+// Gaste um minuto memorizando a linha abaixo
+// Reescreve a linha de código o melhor que puder da memória
+
+ReactDOM.render(<p>Hi, my name is Bob!</p>, document.getElementById("root"))
